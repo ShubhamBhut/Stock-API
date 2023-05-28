7f4e48b274a050ea1f305b74db5b23eea937ae24
Shubham Patel <shubhamprecious1@gmail.com>
2023-05-28 08:20:20 +0530

    basic candlestick chart addition

--- plotly_chart.py
+++ plotly_chart.py
@@ -1,5 +1,6 @@
 import plotly.graph_objects as go
 import pandas as pd
+from stock_data import get_data
 
 df = pd.read_csv(
     "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv"
@@ -13,6 +14,19 @@ fig = go.Figure(
             high=df["AAPL.High"],
             low=df["AAPL.Low"],
             close=df["AAPL.Close"],
+        )
+    ]
+)
+
+ticker = "ITC"
+fig = go.Figure(
+    data = [
+        go.Candlestick(
+            x = get_data(ticker)['date'],
+            open = get_data(ticker)['open'],
+            close = get_data(ticker)['close'],
+            high = get_data(ticker)['high'],
+            low = get_data(ticker)['low']
         )
     ]
 )
--- stock_data.py
+++ stock_data.py
@@ -2,17 +2,22 @@ import yfinance as yf
 
 def get_data(ticker):
     ticker = yf.Ticker(ticker+".NS")
-    data = ticker.history(period="1y")
+    data = ticker.history(period="1y").reset_index()
     opening_prices = data["Open"]
     closing_prices = data["Close"]
     high = data['High']
     low = data['Low']
     volume = data['Volume']
+    date = data['Date']
 
     return {
-       'opening_prices': opening_prices,
-       'closing_prices': closing_prices,
+       'open': opening_prices,
+       'close': closing_prices,
        'high': high,
        'low': low,	
-       'volume': volume	
-    }
+       'volume': volume,
+       'date': date
+   }
+
+
+get_data("itc")
