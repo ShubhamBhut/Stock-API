b3e742f68520e7c9e3c854afc1de5b43a6b1d042
Shubham Patel <shubhamprecious1@gmail.com>
2023-05-28 08:48:33 +0530

    keypoint addition

--- webapp.py
+++ webapp.py
@@ -2,7 +2,14 @@ from scraping import key_points
 from plotly_chart import candlestick_maker
 import streamlit as st
 
+#Input
 ticker = st.text_input("Enter the ticker: ", 'ITC')
+
+#Keypoints
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("Market Cap", f"{key_points(ticker)['market_cap']}")
+
+#Candlestick chart
 st.plotly_chart(candlestick_maker(ticker))
 
 key_points = key_points(ticker)
